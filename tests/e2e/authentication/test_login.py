7e61db1003bd5244e89db7631d959b0020a3c2b2
Davlat Davydov <literally_user@hotmail.com>
2026-06-25 18:55:59 +0300

    tests: some changes

--- tests/e2e/authentication/test_login.py
+++ tests/e2e/authentication/test_login.py
@@ -1,15 +1,20 @@
 from http import HTTPStatus
 
 import pytest
-from dirty_equals import IsInt, IsPartialDict, IsStr
+from dirty_equals import IsInt, IsPartialDataclass, IsPartialDict, IsStr
 from httpx import AsyncClient
 
+from prodik.application.interfaces.repositories import SessionRepository
 from tests.factories.models import UserFactory
 from tests.factories.schemas import LoginRequestFactory
 
 
 @pytest.mark.asyncio
-async def test_login_ok(transport: AsyncClient, user_factory: UserFactory) -> None:
+async def test_login_ok(
+    transport: AsyncClient,
+    user_factory: UserFactory,
+    session_repository: SessionRepository,
+) -> None:
     user_factory_response = await user_factory.build()
 
     request = LoginRequestFactory.build(
@@ -18,9 +23,19 @@ async def test_login_ok(transport: AsyncClient, user_factory: UserFactory) -> No
     )
 
     response = await transport.post("/auth/login", json=request.model_dump())
+    content = response.json()
+
+    session = await session_repository.get_by_host_and_user_id(
+        user_factory_response.user.id, "127.0.0.1"
+    )
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == IsPartialDict(
+    assert session == IsPartialDataclass(
+        user_id=str(user_factory_response.user.id),
+        token=content["refresh_token"],
+        host="127.0.0.1",
+    )
+    assert content == IsPartialDict(
         access_token=IsStr,
         refresh_token=IsStr,
         expires_in=IsInt,
@@ -29,7 +44,9 @@ async def test_login_ok(transport: AsyncClient, user_factory: UserFactory) -> No
 
 @pytest.mark.asyncio
 async def test_login_invalid_password(
-    transport: AsyncClient, user_factory: UserFactory
+    transport: AsyncClient,
+    user_factory: UserFactory,
+    session_repository: SessionRepository,
 ) -> None:
     user_factory_response = await user_factory.build()
 
@@ -40,6 +57,9 @@ async def test_login_invalid_password(
 
     response = await transport.post("/auth/login", json=request.model_dump())
 
+    session = await session_repository.get_by_token(user_factory_response.refresh_token)
+
+    assert session is not None
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == IsPartialDict(
         detail="Invalid email or password",
