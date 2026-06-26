62cec157a944461da10d03cc870a2b057dd19b45
Davlat Davydov <literally_user@hotmail.com>
2026-06-26 07:40:44 +0300

    feat(infrastructure): transaction logging

--- src/prodik/infrastructure/transaction_manager.py
+++ src/prodik/infrastructure/transaction_manager.py
@@ -1,3 +1,6 @@
+import time
+import structlog
+from uuid import uuid4
 from dataclasses import dataclass
 from types import TracebackType
 from typing import override
@@ -6,13 +9,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from prodik.application.interfaces.transaction_manager import TransactionManager
 
+logger = structlog.get_logger()
 
 @dataclass
 class TransactionManagerImpl(TransactionManager):
     _session: AsyncSession
 
     @override
-    async def __aenter__(self) -> None: ...
+    async def __aenter__(self) -> None:
+        self._start = time.perf_counter()
+        self._uuid = uuid4()
+        logger.debug(f"Transaction {self._uuid} opened")
+
 
     @override
     async def __aexit__(
@@ -21,7 +29,17 @@ class TransactionManagerImpl(TransactionManager):
         exc: BaseException | None,
         tb: TracebackType | None,
     ) -> None:
-        if exc:
+        success = True
+        if exc is not None:
+            success = False
             await self._session.rollback()
-            return
-        await self._session.commit()
+        else:
+            await self._session.commit()
+        
+        process_time = time.perf_counter() - self._start
+
+        logger.debug(
+            "Processed transaction",
+            success=success,
+            time=process_time,
+        )
