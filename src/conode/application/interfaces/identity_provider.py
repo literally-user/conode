8ab1f346feff3aef8ccb46d49fbae09767dfbeeb
Davlat Davydov <literally_user@hotmail.com>
2026-08-19 17:36:18 +0300

    feat(application): custom A/A flow

--- src/conode/application/interfaces/identity_provider.py
+++ src/conode/application/interfaces/identity_provider.py
@@ -1,8 +1,9 @@
 from typing import Protocol
 
-from conode.application.interfaces.token_manager import UserMeta
+from conode.application.interfaces.token_managers import UserMeta
 
 
 class IdentityProvider(Protocol):
     def get_current_ip(self) -> str: ...
+    def get_current_refresh_token(self) -> str: ...
     def get_current_user_meta(self) -> UserMeta: ...
