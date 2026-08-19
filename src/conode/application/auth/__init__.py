8882c85304f164075e22614847b755f8e9872e01
Davlat Davydov <literally_user@hotmail.com>
2026-08-19 17:35:18 +0300

    feat(application): custom A/A flow

---
+++ src/conode/application/auth/__init__.py
@@ -0,0 +1,11 @@
+from .login import LoginInteractor, LoginRequestDTO
+from .register import RegisterInteractor, RegisterRequestDTO
+from .shared import AuthorizedResponseDTO
+
+__all__ = (
+    "AuthorizedResponseDTO",
+    "LoginInteractor",
+    "LoginRequestDTO",
+    "RegisterInteractor",
+    "RegisterRequestDTO",
+)
